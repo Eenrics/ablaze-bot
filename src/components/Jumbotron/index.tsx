53ede762f0d80157cea95dfd84fccd86c2a1d159
eenrics <ebenezeresh525@gmail.com>
2023-11-15 12:09:41 +0300

    refactor: add extra structure to the folders

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Hero() {
+function Jumbotron() {
   return (
     <div>
       <h1 className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text transition-all">
@@ -10,4 +10,4 @@ function Hero() {
   );
 }
 
-export default Hero;
+export default Jumbotron;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
 import Ball from "../../components/Ball";
-import Hero from "../../components/Hero";
+import Jumbotron from "../../components/Jumbotron";
 
 function Home() {
   return (
     <>
-      <Hero />
+      <Jumbotron />
       <Ball />
     </>
   );
