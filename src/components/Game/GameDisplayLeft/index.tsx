95d42715490efa37489414fd76da6d05f4acab2e
eenrics <ebenezeresh525@gmail.com>
2023-11-18 06:09:10 +0300

    vercel: merge with main

--- src/components/Game/GameDisplayLeft/index.tsx
+++ src/components/Game/GameDisplayLeft/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import GameTitle from "./GameTitle";
 import GameBoard from "./GameBoard";
 import { signal } from "@preact/signals-react";
