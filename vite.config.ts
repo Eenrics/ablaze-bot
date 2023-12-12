6e0fbd038951145d6a92201b419a4795a3490aa3
Bisrat yenehun <61931149+bisraty@users.noreply.github.com>
2023-12-12 13:25:13 +0300

    feat: added font-type on jackpot component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import path from "path";
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
   plugins: [react()],
 });
