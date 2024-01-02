e313792e0b329700c720a668fef2fa46592261dd
eenrics <ebenezeresh525@gmail.com>
2024-01-02 12:47:00 +0300

    fix: fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
+import mkcert from 'vite-plugin-mkcert'
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from 'fs'
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/bets.et/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/bets.et/fullchain.pem'),
+    }
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-  plugins: [react()],
+  plugins: [react(),],
 });
